45459771b03514825c9ee82d9520e70d49931480
sourena-kazemi <kazemisourena@gmail.com>
2025-10-10 11:10:18 +0330

    update sound command input placeholder when using direct command

--- commands/Sound/sound.go
+++ commands/Sound/sound.go
@@ -37,6 +37,10 @@ func Sound(app fyne.App) {
 				cmd = fmt.Sprintf("amixer sset Master %s", s)
 
 			}
+			result, err := exec.Command("bash", "-c", "awk -F'[][]' '/Left:/ { print $2 }' <(amixer sget Master)").Output()
+			if err == nil {
+				input.SetPlaceHolder(strings.TrimSuffix(strings.TrimSpace(string(result)), "%"))
+			}
 			exec.Command("sh", "-c", cmd).Start()
 			input.SetText("")
 		}
